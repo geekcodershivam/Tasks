41abe388bf532e105e24e53e6fa44566b8f4139c
geekcodershivam <shivamtripathi214@gmail.com>
2021-06-21 18:45:42 +0530

    removed: unused package

--- src/components/HEADER/HEADER.js
+++ src/components/HEADER/HEADER.js
@@ -1,10 +1,9 @@
 import React from 'react';
 import './HEADER.css';
-import Logo from '../../utils/SVG/logo.svg';
 export default function HEADER() {
   return (
     <header className="header">
-      <img src={Logo} alt="FamPay" />
+      <img src="https://i.ibb.co/FWhrCNw/logo.png" alt="FamPay" />
     </header>
   );
 }
